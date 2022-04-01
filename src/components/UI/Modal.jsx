0232c6cd1f5c8c6e6e39a8e99a3ef499287ef1b7
Mostafa <mosi3883@gmail.com>
2022-04-01 13:37:24 +0430

    shopping cart and modal 11-10

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,27 @@
+import classes from './Modal.module.css';
+import ReactDOM from 'react-dom';
+import React from 'react';
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  const parentEl = document.getElementById('overlays');
+
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(<Backdrop />, parentEl)}
+      {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, parentEl)}
+    </React.Fragment>
+  );
+};
+
+export default Modal;
