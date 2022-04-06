b9c92ed6ae934a6405b168ef1fe18a3ce471eb2f
Mostafa <mosi3883@gmail.com>
2022-04-06 17:12:52 +0430

    handling error 17-5

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -6,14 +6,21 @@ import MealItem from './MealItem/MealItem';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
       try {
+        setHttpError(null);
         const response = await fetch('http://localhost:8500/foods');
+        if (!response.ok) {
+          throw new Error(`Something went wrong (${response.statusText})`);
+        }
+
         const data = await response.json();
         setMeals(data);
       } catch (err) {
+        setHttpError(err.message);
       } finally {
         setIsLoading(false);
       }
@@ -25,6 +32,14 @@ const AvailableMeals = () => {
     return (
       <section className={classes['meals-loading']}>
         <p>Loading ...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes['meals-error']}>
+        <p>{httpError}</p>
       </section>
     );
   }
